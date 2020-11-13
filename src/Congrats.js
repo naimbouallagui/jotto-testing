9c0b1163f7f702e297e395ed25fb70cb1f2c3d93
Naim Bouallagui <naim.bouallagui.chantier@gmail.com>
2020-11-13 23:48:38 +0100

    Add Congtas component and test utils

---
+++ src/Congrats.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @param {object} props - React props
+ * @returns {JSX.Element} - Rendered component(or null if `success` prop is null)
+ */
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
