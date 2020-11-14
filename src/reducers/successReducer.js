3e1acda8967ad55113bb484283be8312b06f1a92
Naim Bouallagui <naim.bouallagui.chantier@gmail.com>
2020-11-14 10:38:38 +0100

    Setup Redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+export default (state, action) =>{
+    return null
+}
