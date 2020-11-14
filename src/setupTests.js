959af8158446a4b9c6e816f71ea24ee273b6c4a1
Naim Bouallagui <naim.bouallagui.chantier@gmail.com>
2020-11-14 09:31:35 +0100

    add guessedwords component

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-
+import '@testing-library/jest-dom';
 import  Enzyme, {shallow} from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16'
 Enzyme.configure({adapter: new EnzymeAdapter()})
