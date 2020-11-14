15f08bdcf4b4e888999dcaa9ded51e0708fa4637
Naim Bouallagui <naim.bouallagui.chantier@gmail.com>
2020-11-14 11:15:55 +0100

    write code and test for successReducer

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,7 +1,7 @@
 import {correctGuess, actionTypes} from './'
 
 describe('correctGuess', ()=>{
-    test('returns an action with type ``CORRECT_GUESS', ()=>{
+    test('returns an action with type `CORRECT_GUESS`', ()=>{
         const action = correctGuess();
         // toBe for immutable(number, string,..), toEqual for mutable({}, [])
         expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
