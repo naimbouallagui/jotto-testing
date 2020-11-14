2309c169734708cf8b0f19a4da64f4dc614fb8d9
Naim Bouallagui <naim.bouallagui.chantier@gmail.com>
2020-11-14 12:24:00 +0100

    write test of input component for success=true

--- src/Input.test.js
+++ src/Input.test.js
@@ -34,13 +34,23 @@ describe('render', ()=> {
         
     })
     describe('word has been guessed', () => {
+        let wrapper;
+        beforeEach(()=>{
+            const initialState = {success: true};
+            wrapper = setup(initialState);
+        })
         test('renders component without error ', () => {
+            const component = findByTestAttr(wrapper,'component-input');
+            expect(component.length).toBe(1);
+        })
+        test('renders does not input box ', () => {
+            const inputBox = findByTestAttr(wrapper,'input-box');
+            expect(inputBox.length).toBe(0);
             
         })
-        test('renders does not render input box ', () => {
-            
-        })
-        test('renders does not render submit button ', () => {
+        test('renders does not submit button ', () => {
+            const submitButton = findByTestAttr(wrapper,'submit-button');
+            expect(submitButton.length).toBe(0);
             
         })
     })
